9e92b994ff5d15c97eb55930ce7a9fae8b49b839
Delnegend <kiennguyen19323@gmail.com>
2026-08-28 23:37:26 +0700

    feat(segotep-digital): download pre-built binaries with source fallback

--- Formula/segotep-digital.rb
+++ Formula/segotep-digital.rb
@@ -1,8 +1,7 @@
 ﻿class SegotepDigital < Formula
   desc "Driver and daemon for Segotep Ice Moon / Digital series AIO CPU coolers"
   homepage "https://github.com/Delnegend/segotep-digital"
-  url "https://github.com/Delnegend/segotep-digital/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e995881b6655161a79f9318b6dbf972d203af1a092b4a2b413102089868de3df"
+  version "0.1.0"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/Delnegend/segotep-digital.git", branch: "main"
 
@@ -11,13 +10,33 @@
     strategy :github_releases
   end
 
-  depends_on "rust" => :build
   depends_on :linux
 
+  on_linux do
+    on_intel do
+      url "https://github.com/Delnegend/segotep-digital/releases/download/v0.1.0/segotep-digital-v0.1.0-linux-amd64.tar.xz"
+      sha256 "712b5ed037db5678b7ec3fe08047c9255d8e8ef0d04622f73c3ed189385e9c7e"
+    end
+    on_arm do
+      url "https://github.com/Delnegend/segotep-digital/releases/download/v0.1.0/segotep-digital-v0.1.0-linux-arm64.tar.xz"
+      sha256 "11b03908393b7275ed1ab4cf2d0e33459b15a5772388b8e4500abd21e691c411"
+    end
+  end
+
+  head do
+    depends_on "rust" => :build
+  end
+
   def install
-    system "cargo", "install", *std_cargo_args
-    pkgshare.install "udev/99-segotep.rules"
-    pkgshare.install "systemd/segotep-digital.service"
+    if build.head? || File.exist?("Cargo.toml")
+      system "cargo", "install", *std_cargo_args
+      pkgshare.install "udev/99-segotep.rules" if File.exist?("udev/99-segotep.rules")
+      pkgshare.install "systemd/segotep-digital.service" if File.exist?("systemd/segotep-digital.service")
+    else
+      bin.install "segotep-digital"
+      pkgshare.install "99-segotep.rules" if File.exist?("99-segotep.rules")
+      pkgshare.install "segotep-digital.service" if File.exist?("segotep-digital.service")
+    end
   end
 
   def caveats
